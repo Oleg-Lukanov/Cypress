84b67e500673e95ca11f497d686552111c8170bc
Oleh Lukainchuk <oleeluk@gmail.com>
2024-07-21 16:57:49 +0300

    Config files for different resolutions have been created

---
+++ cypress/config/desktop.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    projectId: "rdsaot",
+    "video": true,
+    "screenshotOnRunFailure": true,
+    baseUrl: 'https://conduit.realworld.how',
+    viewportWidth: 1920,
+    viewportHeight: 1080,
+  },
+});
